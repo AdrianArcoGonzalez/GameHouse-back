27c6d8bb1ef6d5dd227e4f143cdd104147b7b64f
Adrian Arco <adrian.arco.gonzalez@gmail.com>
2022-08-29 18:49:40 +0200

    add general error middleware

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import chalk from "chalk";
 import morgan from "morgan";
 import connectDatabase from "./database";
 import { app, startServer } from "./server/startServer";
+import generalError from "./middlewares/error";
 
 const debug = Debug("GAMES:");
 const port = process.env.PORT ?? 4500;
@@ -19,6 +20,8 @@ app.use((req, _res, next) => {
   debug(chalk.blue(`A request arrived to ${req.url}`));
   next();
 });
+
+app.use("/", generalError);
 
 (async () => {
   try {
