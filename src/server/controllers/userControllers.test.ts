4ff339738a801a8312cb26397dc5fb8356557f75
Adrian Arco <adrian.arco.gonzalez@gmail.com>
2022-08-29 22:51:52 +0200

    add catch error test registerUser

--- src/server/controllers/userControllers.test.ts
+++ src/server/controllers/userControllers.test.ts
@@ -5,28 +5,59 @@ import registerUser from "./userControllers";
 
 describe("Given a userController controller", () => {
   describe("When it invoked with registerUser method", () => {
+    const newUser: UserRegister = {
+      birthdate: new Date(),
+      email: "",
+      image: "",
+      location: "",
+      name: "",
+      password: "",
+      username: "",
+    };
+    const status = 200;
+
+    const req: Partial<Request> = { body: newUser };
+
     test("Then it should call the status method with a 200", async () => {
-      const user: UserRegister = {
-        birthdate: new Date(),
-        email: "",
-        image: "",
-        location: "",
-        name: "",
-        password: "",
-        username: "",
-      };
-      const status = 200;
+      User.create = jest.fn().mockResolvedValue(newUser);
       const next: NextFunction = jest.fn();
-      const req: Partial<Request> = { body: user };
       const res: Partial<Response> = {
         status: jest.fn().mockReturnThis(),
         json: jest.fn(),
       };
-      User.create = jest.fn().mockResolvedValue(user);
 
       await registerUser(req as Request, res as Response, next as NextFunction);
 
       expect(res.status).toHaveBeenCalledWith(status);
+    });
+
+    test("And it should call the method json with the newUser created", async () => {
+      User.create = jest.fn().mockResolvedValue(newUser);
+
+      const next: NextFunction = jest.fn();
+      const res: Partial<Response> = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn().mockResolvedValue({ user: newUser }),
+      };
+
+      await registerUser(req as Request, res as Response, next as NextFunction);
+
+      expect(res.json).toHaveBeenCalledWith({ user: newUser });
+    });
+
+    test("And if it reject the create method it should call the next function with the a custom error", async () => {
+      const error = new Error();
+      User.create = jest.fn().mockRejectedValue(error);
+
+      const next: NextFunction = jest.fn();
+      const res: Partial<Response> = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn().mockResolvedValue({ user: newUser }),
+      };
+
+      await registerUser(req as Request, res as Response, next as NextFunction);
+
+      expect(next).toHaveBeenCalledWith(error);
     });
   });
 });
