1e1bb6684a0e263a216d3375320b5f1c9a04a341
Adrian Arco <adrian.arco.gonzalez@gmail.com>
2022-09-01 11:37:10 +0200

    add joi validation on controller

--- src/server/middlewares/error.ts
+++ src/server/middlewares/error.ts
@@ -1,6 +1,7 @@
 import chalk from "chalk";
 import { debug } from "console";
 import { NextFunction, Request, Response } from "express";
+import { ValidationError } from "express-validation";
 import { CustomError } from "../../interfaces/interfaces";
 
 const generalError = (
@@ -10,9 +11,16 @@ const generalError = (
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   _next: NextFunction
 ) => {
-  const errorCode = error.statusCode;
-  const publicMessage = error.publicmessage;
-  const privateMessage = error.privatemessage;
+  let errorCode = error.statusCode;
+  let publicMessage = error.publicmessage;
+  let privateMessage = error.privatemessage;
+
+  if (error instanceof ValidationError) {
+    errorCode = 400;
+    publicMessage = "Invalid data";
+    privateMessage = error.details.body[0].message;
+    debug(chalk.blue("Invalidation request data:"));
+  }
 
   res.status(errorCode).json({ publicMessage });
   debug(chalk.red(privateMessage));
