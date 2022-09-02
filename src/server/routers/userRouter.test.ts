5b7106887f944b0c23e6d508b0d4339c8cb436d7
Adrian Arco <adrian.arco.gonzalez@gmail.com>
2022-09-03 01:23:34 +0200

    add supertest happy path

--- src/server/routers/userRouter.test.ts
+++ src/server/routers/userRouter.test.ts
@@ -3,8 +3,19 @@ import { MongoMemoryServer } from "mongodb-memory-server";
 import mongoose from "mongoose";
 import app from "../index";
 import connectDatabase from "../../database";
+import User from "../../database/models/User";
 
 let mongoServer: MongoMemoryServer;
+const userBaseData = {
+  birthdate: new Date(),
+  email: "adrian@gmail.com",
+  image: "adrianImage.jpg",
+  location: "barcelona",
+  name: "adrian",
+  repeatPassword: "123456789",
+  password: "123456789",
+  username: "adrian89",
+};
 
 beforeAll(async () => {
   mongoServer = await MongoMemoryServer.create();
@@ -16,6 +27,9 @@ beforeAll(async () => {
 afterAll(async () => {
   await mongoose.connection.close();
   await mongoServer.stop();
+});
+afterEach(() => {
+  User.deleteMany({});
 });
 
 const newUser = {
@@ -31,7 +45,7 @@ const userJson = JSON.stringify(newUser);
 const user = userJson;
 
 describe("Given a register endpoint", () => {
-  describe("When it receive a request with method post and the correct user data", () => {
+  describe("When it receive a request with method post on register and the correct user data", () => {
     test("Then it should response with status 201", async () => {
       const expectedStatus = 201;
       await request(app)
@@ -49,6 +63,18 @@ describe("Given a register endpoint", () => {
       const user2 = {};
       await request(app)
         .post("/games/users/register")
+        .send(user2)
+        .expect(expectedStatus);
+    });
+  });
+  describe("When it receive a request with post on login with correct data", () => {
+    test("Then it should response with status 200 and a token", async () => {
+      await User.create(userBaseData);
+
+      const expectedStatus = 200;
+      const user2 = { username: "adrian", password: "12345" };
+      await request(app)
+        .post("/games/users/login")
         .send(user2)
         .expect(expectedStatus);
     });
