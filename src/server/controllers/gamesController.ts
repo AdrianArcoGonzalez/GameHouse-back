4e4d8d10b4c13d0d5275b43450cb599336f940f6
Adrian Arco <adrian.arco.gonzalez@gmail.com>
2022-09-04 09:42:16 +0200

    add getAllGames function to games controller

---
+++ src/server/controllers/gamesController.ts
@@ -0,0 +1,31 @@
+import chalk from "chalk";
+import { NextFunction, Request, Response } from "express";
+import Debug from "debug";
+import customError from "../../utils/customError";
+import Games from "../../database/models/Game";
+
+const debug = Debug("ROBOTS:Controllers");
+
+export const getAllGames = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  debug(chalk.yellow("Received a getAllGames req"));
+  try {
+    const AllGames = await Games.find();
+    debug(chalk.yellow("Sending a response from getAllGames"));
+
+    res.status(200).json({ AllGames });
+  } catch {
+    const errorGetAll = customError(
+      500,
+      "Conection to database is down",
+      "Cannot reach this request"
+    );
+
+    next(errorGetAll);
+  }
+};
+
+export default getAllGames;
