91358fa80abc38d19b987fb320fffb67b5112e4c
Adrian Arco <adrian.arco.gonzalez@gmail.com>
2022-09-08 18:54:09 +0200

    add create game controller and route

--- src/server/routers/gamesRouter.ts
+++ src/server/routers/gamesRouter.ts
@@ -1,15 +1,20 @@
 import express from "express";
+import multer from "multer";
 import {
+  createGame,
   deleteOne,
   getAllGames,
   getById,
   getOwnerGames,
 } from "../controllers/gamesController";
 import authentication from "../middlewares/auth";
+import parserJson from "../middlewares/parserJson";
 
+const upload = multer({ dest: "uploads" });
 const gamesRouter = express.Router();
 
 gamesRouter.get("/", getAllGames);
+gamesRouter.post("/", upload.single("image"), parserJson, createGame);
 gamesRouter.get("/my-collection/:owner", authentication, getOwnerGames);
 gamesRouter.get("/:id", authentication, getById);
 gamesRouter.delete("/", authentication, deleteOne);
