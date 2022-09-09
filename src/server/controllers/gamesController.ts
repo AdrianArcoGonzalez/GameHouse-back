a5eb96fa4f5c7ec274136705bb251a75c361f2da
Adrian Arco <adrian.arco.gonzalez@gmail.com>
2022-09-09 20:07:58 +0200

    add test to supabase

--- src/server/controllers/gamesController.ts
+++ src/server/controllers/gamesController.ts
@@ -94,7 +94,6 @@ export const createGame = async (
 ) => {
   const game = req.body;
   try {
-    debugger;
     validate(gameSchema, {}, { abortEarly: false });
     const gameCreated = await Game.create(game);
     res.status(201).json({ game: gameCreated });
